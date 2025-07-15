6f6e24f9a64800dc9035514f24085cfe951b1111
dcvz <david@dcvz.io>
2025-07-15 23:33:38 +0200

    fix(transform): don’t constrain pre_updates to main (#86)

--- godot-bevy/src/plugins/transforms/sync_systems.rs
+++ godot-bevy/src/plugins/transforms/sync_systems.rs
@@ -36,7 +36,6 @@ pub fn post_update_godot_transforms_3d(
     }
 }
 
-#[main_thread_system]
 pub fn pre_update_godot_transforms_3d(
     config: Res<crate::plugins::core::GodotTransformConfig>,
     mut entities: Query<(&mut Transform3D, &mut GodotNodeHandle), With<Node3DMarker>>,
@@ -90,7 +89,6 @@ pub fn post_update_godot_transforms_2d(
     }
 }
 
-#[main_thread_system]
 pub fn pre_update_godot_transforms_2d(
     config: Res<crate::plugins::core::GodotTransformConfig>,
     mut entities: Query<(&mut Transform2D, &mut GodotNodeHandle), With<Node2DMarker>>,
